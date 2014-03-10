6c30581ce996784152da25aff3dc4fabb66cc9b5
Michal Cwienczek <michael.cwienczek@gmail.com>
2014-03-10 12:34:44 +0100

    Init commit

---
+++ Computation Cluster/TaskManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computation_Cluster
+{
+    /// <summary>
+    /// The task manager.
+    /// </summary>
+    public class TaskManager
+    {
+    }
+}
