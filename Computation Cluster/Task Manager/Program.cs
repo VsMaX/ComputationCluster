f0c930c9f3365c92657b451ac3057ed3f1e3836b
chmielp <piotrchmiel90@gmail.com>
2014-03-31 07:21:29 +0200

    TaskManager Class added

---
+++ Computation Cluster/Task Manager/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var node = new TaskManager("127.0.0.1", 22222);
+            Console.ReadKey();
+            node.RegisterAtServer();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Computation Cluster/Task Manager/TaskManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Communication_Library;
+
+namespace Task_Manager
+{
+    public class TaskManager : BaseNode
+    {
+        private int serverPort;
+        private string serverIp;
+        private CommunicationModule communicationModule;
+
+        public TaskManager(string serverIp, int serverPort)
+        {
+            communicationModule = new CommunicationModule(serverIp, serverPort);
+        }
+
+        public void RegisterAtServer()
+        {
+            var registerMessage = new RegisterMessage()
+            {
+                ParallelThreads = 8,
+                SolvableProblems = new string[] { "TSP" },
+                Type = RegisterType.TaskManager
+            };
+            var messageString = SerializeMessage(registerMessage);
+            var messageBytes = CommunicationModule.ConvertStringToData(messageString);
+            communicationModule.Connect();
+            communicationModule.SendData(messageBytes);
+            var response = communicationModule.ReceiveData();
+            Trace.WriteLine("Response: " + response.ToString());
+            var deserializedResponse = DeserializeMessage<RegisterResponseMessage>(response);
+            Trace.WriteLine("Response has been deserialized");
+        }
+
+        public int NodeId { get; set; }
+    }
+
+}
