927932f3dbf64633b68bd80739ef9f6cc06e2498
Michal Cwienczek <michael.cwienczek@gmail.com>
2014-04-23 01:26:15 +0200

    ICommunicationModule

---
+++ Computation Cluster/Communication Library/ICommunicationModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Communication_Library
+{
+    public interface ICommunicationModule
+    {
+        void StartListening();
+        string ReceiveData();
+        void SendData(string message);
+        void SetupListening();
+        void SetupConnecting();
+        void Connect();
+        void CloseConnection();
+    }
+}
