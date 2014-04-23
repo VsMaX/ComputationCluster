ee1fa5e1748b583bbe8a464ea53db8618e6e5729
Pawel <pawel.mitrus@gmail.com>
2014-04-23 13:24:15 +0200

    Communication Module with sync communication v2

--- Computation Cluster/Communication Library/CommunicationModule.cs
+++ Computation Cluster/Communication Library/CommunicationModule.cs
@@ -79,7 +79,7 @@ namespace Communication_Library
             Trace.WriteLine("Socket set up for listening");
         }
 
-        private void StartListening()
+        public void StartListening()
         {
             // Places a Socket in a listening state and specifies the maximum 
             // Length of the pending connections queue 
@@ -207,44 +207,9 @@ namespace Communication_Library
             }
         }
 
-        ///////////////////////////////////////////////////////////////////////////////////////
-
-        public void SendDataAsync(byte[] message)
-        {
-            // Sends data to a connected Socket. 
-            int bytesCount = 0;
-            while (bytesCount != message.Length)
-            {
-                bytesCount += this.socket.Send(message);
-                Trace.WriteLine("Sent " + bytesCount.ToString() + " bytes to server");
-            }
-        }
-
-        public string ReceiveDataAsync()
-        {
-            //while (true)
-            //{
-                // Set the event to nonsignaled state.
-                //allDone.Reset();
-
-                // Start an asynchronous socket to listen for connections.
-                Console.WriteLine("Waiting for a connection...");
-                this.socket.BeginAccept(new AsyncCallback(this.AcceptCallback), this.socket);
-                //allDone.WaitOne();
-                // Wait until a connection is made before continuing.
-            //}
-
-            return String.Empty;
-        }
-
-        //public void Dispose()
-        //{
-        //    Disconnect();
-        //}
-
         public static string ConvertDataToString(byte[] buffer, int bytesRead)
         {
-            if(bytesRead > 0)
+            if (bytesRead > 0)
                 return Encoding.UTF8.GetString(buffer, 0,
                         bytesRead);
             return String.Empty;
@@ -255,141 +220,175 @@ namespace Communication_Library
             return Encoding.UTF8.GetBytes(response);
         }
 
+        ///////////////////////////////////////////////////////////////////////////////////////
 
-        public void AcceptCallback(IAsyncResult ar)
+        //public void SendDataAsync(byte[] message)
+        //{
+        //    // Sends data to a connected Socket. 
+        //    int bytesCount = 0;
+        //    while (bytesCount != message.Length)
+        //    {
+        //        bytesCount += this.socket.Send(message);
+        //        Trace.WriteLine("Sent " + bytesCount.ToString() + " bytes to server");
+        //    }
+        //}
+
+        //public string ReceiveDataAsync()
+        //{
+        //    //while (true)
+        //    //{
+        //        // Set the event to nonsignaled state.
+        //        //allDone.Reset();
+
+        //        // Start an asynchronous socket to listen for connections.
+        //        Console.WriteLine("Waiting for a connection...");
+        //        this.socket.BeginAccept(new AsyncCallback(this.AcceptCallback), this.socket);
+        //        //allDone.WaitOne();
+        //        // Wait until a connection is made before continuing.
+        //    //}
+
+        //    return String.Empty;
+        //}
+
+        ////public void Dispose()
+        ////{
+        ////    Disconnect();
+        ////}
+
+        //public void AcceptCallback(IAsyncResult ar)
+        //{
+        //    Trace.WriteLine("Accepted callback");
+        //    allDone.Set();
+        //    Thread.Sleep(2000);
+
+        //    // A new Socket to handle remote host communication 
+        //    try
+        //    {
+        //        this.HandleCommunication(ar);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Trace.WriteLine(ex.ToString());
+        //    }
+        //}
+
+        //public void HandleCommunication(IAsyncResult ar)
+        //{
+        //    Socket listener = null;
+
+        //    // Receiving byte array 
+        //    byte[] buffer = new byte[1024];
+
+        //    // Get Listening Socket object 
+        //    listener = (Socket)ar.AsyncState;
+
+        //    // Create a new socket 
+        //    this.handler = listener.EndAccept(ar);
+
+        //    // Using the Nagle algorithm 
+        //    this.handler.NoDelay = false;
+
+        //    // Creates one object array for passing data 
+        //    object[] obj = new object[2];
+        //    obj[0] = buffer;
+        //    obj[1] = this.handler;
+
+        //    // Begins to asynchronously receive data 
+        //    this.handler.BeginReceive(
+        //        buffer,        // An array of type Byt for received data
+        //        0,             // The zero-based position in the buffer
+        //        buffer.Length, // The number of bytes to receive
+        //        SocketFlags.None,// Specifies send and receive behaviors
+        //        new AsyncCallback(this.ReceiveCallback),//An AsyncCallback delegate
+        //        obj            // Specifies infomation for receive operation
+        //        );
+
+        //    // Begins an asynchronous operation to accept an attempt 
+        //    AsyncCallback aCallback = new AsyncCallback(this.AcceptCallback);
+        //    listener.BeginAccept(aCallback, listener);
+        //}
+
+        //public void ReceiveCallback(IAsyncResult ar)
+        //{
+        //    try
+        //    {
+        //        string content = this.ReadMessage(ar);
+        //        Trace.WriteLine("Received message: \n" + content);
+
+        //        //var response = this.ProcessMessage(content, this.GetMessageName(content));
+        //        var response = String.Empty;
+        //        byte[] bytes = CommunicationModule.ConvertStringToData(response);
+        //        handler.BeginSend(bytes, 0, bytes.Length, 0, new AsyncCallback(SendCallback), handler);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Trace.WriteLine(ex.ToString());
+        //    }
+        //}
+
+        //public string ReadMessage(IAsyncResult ar)
+        //{
+        //    // Fetch a user-defined object that contains information 
+        //    object[] obj = new object[2];
+        //    obj = (object[])ar.AsyncState;
+
+        //    // Received byte array 
+        //    byte[] buffer = (byte[])obj[0];
+
+        //    // A Socket to handle remote host communication. 
+        //    handler = (Socket)obj[1];
+
+        //    // Received message 
+        //    string content = string.Empty;
+
+        //    // The number of bytes received. 
+        //    int bytesRead = handler.EndReceive(ar);
+
+        //    return CommunicationModule.ConvertDataToString(buffer, bytesRead);
+        //}
+
+        //private static void SendCallback(IAsyncResult ar)
+        //{
+        //    try
+        //    {
+        //        // Retrieve the socket from the state object.
+        //        Socket handler = (Socket)ar.AsyncState;
+
+        //        // Complete sending the data to the remote device.
+        //        int bytesSent = handler.EndSend(ar);
+        //        Console.WriteLine("Sent {0} bytes back.", bytesSent);
+
+        //        handler.Shutdown(SocketShutdown.Both);
+        //        handler.Close();
+
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Trace.WriteLine(e.ToString());
+        //    }
+        //}
+
+        //private string GetMessageName(string message)
+        //{
+        //    XmlDocument doc = new XmlDocument();
+        //    try
+        //    {
+        //        doc.LoadXml(message);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Trace.WriteLine("Error parsing xml document: " + message + "exception: " + ex.ToString());
+        //        return String.Empty;
+
+        //        //TODO logowanie
+        //    }
+        //    XmlElement root = doc.DocumentElement;
+        //    return root.Name;
+        //}
+
+        public void Dispose()
         {
-            Trace.WriteLine("Accepted callback");
-            allDone.Set();
-            Thread.Sleep(2000);
-
-            // A new Socket to handle remote host communication 
-            try
-            {
-                this.HandleCommunication(ar);
-            }
-            catch (Exception ex)
-            {
-                Trace.WriteLine(ex.ToString());
-            }
+            throw new NotImplementedException();
         }
-
-        public void HandleCommunication(IAsyncResult ar)
-        {
-            Socket listener = null;
-
-            // Receiving byte array 
-            byte[] buffer = new byte[1024];
-
-            // Get Listening Socket object 
-            listener = (Socket)ar.AsyncState;
-
-            // Create a new socket 
-            this.handler = listener.EndAccept(ar);
-
-            // Using the Nagle algorithm 
-            this.handler.NoDelay = false;
-
-            // Creates one object array for passing data 
-            object[] obj = new object[2];
-            obj[0] = buffer;
-            obj[1] = this.handler;
-
-            // Begins to asynchronously receive data 
-            this.handler.BeginReceive(
-                buffer,        // An array of type Byt for received data
-                0,             // The zero-based position in the buffer
-                buffer.Length, // The number of bytes to receive
-                SocketFlags.None,// Specifies send and receive behaviors
-                new AsyncCallback(this.ReceiveCallback),//An AsyncCallback delegate
-                obj            // Specifies infomation for receive operation
-                );
-
-            // Begins an asynchronous operation to accept an attempt 
-            AsyncCallback aCallback = new AsyncCallback(this.AcceptCallback);
-            listener.BeginAccept(aCallback, listener);
-        }
-
-        public void ReceiveCallback(IAsyncResult ar)
-        {
-            try
-            {
-                string content = this.ReadMessage(ar);
-                Trace.WriteLine("Received message: \n" + content);
-
-                //var response = this.ProcessMessage(content, this.GetMessageName(content));
-                var response = String.Empty;
-                byte[] bytes = CommunicationModule.ConvertStringToData(response);
-                handler.BeginSend(bytes, 0, bytes.Length, 0, new AsyncCallback(SendCallback), handler);
-            }
-            catch (Exception ex)
-            {
-                Trace.WriteLine(ex.ToString());
-            }
-        }
-
-        public string ReadMessage(IAsyncResult ar)
-        {
-            // Fetch a user-defined object that contains information 
-            object[] obj = new object[2];
-            obj = (object[])ar.AsyncState;
-
-            // Received byte array 
-            byte[] buffer = (byte[])obj[0];
-
-            // A Socket to handle remote host communication. 
-            handler = (Socket)obj[1];
-
-            // Received message 
-            string content = string.Empty;
-
-            // The number of bytes received. 
-            int bytesRead = handler.EndReceive(ar);
-
-            return CommunicationModule.ConvertDataToString(buffer, bytesRead);
-        }
-
-        private static void SendCallback(IAsyncResult ar)
-        {
-            try
-            {
-                // Retrieve the socket from the state object.
-                Socket handler = (Socket)ar.AsyncState;
-
-                // Complete sending the data to the remote device.
-                int bytesSent = handler.EndSend(ar);
-                Console.WriteLine("Sent {0} bytes back.", bytesSent);
-
-                handler.Shutdown(SocketShutdown.Both);
-                handler.Close();
-
-            }
-            catch (Exception e)
-            {
-                Trace.WriteLine(e.ToString());
-            }
-        }
-
-        private string GetMessageName(string message)
-        {
-            XmlDocument doc = new XmlDocument();
-            try
-            {
-                doc.LoadXml(message);
-            }
-            catch (Exception ex)
-            {
-                Trace.WriteLine("Error parsing xml document: " + message + "exception: " + ex.ToString());
-                return String.Empty;
-
-                //TODO logowanie
-            }
-            XmlElement root = doc.DocumentElement;
-            return root.Name;
-        }
-
-
-
-
-       
     }
 }
