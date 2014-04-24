1732bd2168e7d141eb005c71fb743d64aea5d6fd
Michal Cwienczek <michael.cwienczek@gmail.com>
2014-04-24 15:32:26 +0200

    Postsharp test

---
+++ Computation Cluster/Communication Library/HandleExceptionAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PostSharp.Aspects;
+
+namespace Communication_Library
+{
+    [Serializable]
+    public class HandleExceptionAttribute : OnExceptionAspect
+    {
+        public override void OnException(MethodExecutionArgs args)
+        {
+            args.FlowBehavior = FlowBehavior.Continue;
+            //TODO logging
+            Trace.WriteLine(args.Exception.ToString());
+            base.OnException(args);
+        }
+    }
+}
