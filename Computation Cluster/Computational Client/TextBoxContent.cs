e9aefd7a0c6b4fcbd4d39fcc543f920af016780c
Pawel <pawel.mitrus@gmail.com>
2014-05-05 10:28:01 +0200

    responsive CC with problem loading

---
+++ Computation Cluster/Computational Client/TextBoxContent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computational_Client
+{
+    public class TextBoxContent : INotifyPropertyChanged
+    {
+        public string textBoxContent;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public TextBoxContent(){}
+
+        public TextBoxContent(string value)
+        {
+            this.textBoxContent = value;
+        }
+
+        public string Content
+        {
+            get
+            {
+                return this.textBoxContent; 
+            }
+
+            set
+            {
+                this.textBoxContent = value;
+                OnPropertyChanged("TextBoxContent");
+            }
+        }
+
+        private void OnPropertyChanged(string textBoxContent)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(textBoxContent));
+            }
+        }
+    }
+}
